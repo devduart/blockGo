a038245c826b1738d6fc05ac6e1b685d93e4e763
devDuart <walysson.duarte@gmail.com>
2025-05-02 11:59:18 -0300

    feat: create main application to initialize and print blockchain

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/devduart/blockGo/blockchain"
+)
+
+func main() {
+	// Cria uma nova blockchain com o bloco gênesis
+	bc := blockchain.NewBlockchain()
+
+	// Adiciona dois blocos novos com dados de transações simuladas
+	bc = blockchain.AddBlock(bc, "Send 1 GoHorse to Alice")
+	bc = blockchain.AddBlock(bc, "Send 2 GoHorse to Bob")
+
+	// Itera sobre a blockchain e imprime os dados de cada bloco
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n\n", block.Hash)
+	}
+}
