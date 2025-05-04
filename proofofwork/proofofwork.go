97383a9d34f7a21b4f81ecaca914387825a9a436
Alexandre Ferreira <alexandreferreira@Alexandres-MacBook-Pro.local>
2025-05-04 18:42:53 -0300

    feat: impl proof of work

---
+++ proofofwork/proofofwork.go
@@ -0,0 +1,21 @@
+package proofofwork
+
+import (
+	"math/big"
+)
+
+const targetBits = 24
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
